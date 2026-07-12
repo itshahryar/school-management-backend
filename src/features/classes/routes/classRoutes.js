2e2ac5c628744b052c9bc65876135cf9566f354e
itshahryar <shahryaramjadmos@gmail.com>
2026-07-12 02:33:44 -0700

    ownerOnly Routing

--- src/features/classes/routes/classRoutes.js
+++ src/features/classes/routes/classRoutes.js
@@ -7,11 +7,11 @@ const {
   updateClassValidation,
 } = require('../validations/classValidation');
 const { authenticate } = require('../../../middleware/auth');
-const { adminOrHigher } = require('../../../middleware/authorization');
+const { ownerOnly } = require('../../../middleware/authorization');
 
 const router = express.Router();
 
-router.use(authenticate, adminOrHigher);
+router.use(authenticate, ownerOnly);
 
 router.get('/', listClassesValidation, classController.listClasses);
 router.post('/', createClassValidation, classController.createClass);
